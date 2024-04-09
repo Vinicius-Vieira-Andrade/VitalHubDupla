ba0986e7be33555e778d4cf0b206076414a3e980
Mikael Souza Da França Falcão <51915337801@info.local>
2024-04-09 17:22:17 -0300

    09/04 - Desenvolvendo a função Buscar usuário.

--- vitalHub - Mikael/src/screens/PatientProfile/PatientProfile.js
+++ vitalHub - Mikael/src/screens/PatientProfile/PatientProfile.js
@@ -20,16 +20,17 @@ import {
 } from "../../components/Button/Button";
 import { userDecodeToken, userTokenLogout } from "../../utils/Auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
 
 export const PatientProfile = ({ navigation }) => {
 
   const [paciente, setPaciente] = useState({});
   const [medico, setMedico] = useState({});
-  const [datanascimento, setDataNascimento] = useState({});
-  const [cpf, setCpf] = useState({});
-  const [logradouro, setLogradouro] = useState({});
-  const [cep, setCep] = useState({});
-  const [cidade, setCidade] = useState({});
+  const [datanascimento, setDataNascimento] = useState("");
+  const [cpf, setCpf] = useState("");
+  const [logradouro, setLogradouro] = useState("");
+  const [cep, setCep] = useState("");
+  const [cidade, setCidade] = useState("");
 
   const [user, setUser] = useState({});
 
@@ -37,66 +38,92 @@ export const PatientProfile = ({ navigation }) => {
   async function profileLoad() {
     const token = await userDecodeToken();
 
-    if (token) {
-      // console.log("funcionou!");
+    if (token !== null) {
+      
+      // await searchUser(token);
       setUser(token);
     }
+
+    else{
+      console.error(error);
+      // searchUser
+    }
   }
 
-  // NÃO ESTÁ CAINDO NO ELSE, MAS ESTÁ DANDO ERRO 401 ( PROVAVELMETE NÃO ESTÁ RECEBENDO O ID DE FORMA CORRETA )
-  async function searchUser() {
-    const token = await userDecodeToken();
-    console.log('O usuário atual é -',token.role, '-',token.name);
-    
-    const id = token.id;
+  // 
+  // async function searchUser(token) {
+  //   const url = (token.role == "Medico" ? "Medicos" : "Pacientes");
+  //   console.log('O usuário atual é -',token.role, '-',token.name, '-', token.id);
 
+  //   console.log(token)
+
+  //   if (token.role !== null) {
+  //     const response = await api.get(`/${url}/BuscarPorId?id=${token.id}`)
+  
+  //     setPaciente(response.data)
+  //     // setMedico(responseData)
+  //     // setDataNascimento(responseData.paciente.datanascimento)
+  //     // setCpf(responseData)
+  //     setLogradouro(response.data.logradouro)
+  //     // setCep(responseData.endereco.cep)
+  //     setCidade(response.data.localidade)
+  //   } else {
+  //     console.log('Error');
+  //   }
+    
+  // }
+    
     // SE O USUÁRIO FOR PACIENTE 
-    if (token.role === "Paciente") {
-      try {
-        console.log('Funcionou, o usuário retornado é paciente') 
-        const response = await api.get(`/Pacientes/BuscarPorId?id=${user.id}`);
+    // if (token.role === "Paciente") {
+    //   try {
+    //     // console.log('Funcionou, o usuário retornado é Paciente') 
+    //     // console.log('-', token.role, '-',user.id)
+    //     // const response = 
+    //     await api.get(`/${url}/BuscarPorId?id=${user.id}`)
+    //     .then( response => {
+    //       setPaciente(response.data);
+          
+    //     })
 
-        console.log(user.id)
 
-        setPaciente(response.data);
-        setDataNascimento(response.data)
-        setCpf(response.data)
-        setLogradouro(response.data.endereco.logradouro);
-        setCep(response.data.endereco.cep);
-        setCidade(response.data.endereco.cidade);
+    //     // setDataNascimento(response.data)
+    //     // setCpf(response.data)
+    //     // setLogradouro(response.data.endereco.logradouro);
+    //     // setCep(response.data.endereco.cep);
+    //     // setCidade(response.data.endereco.cidade);
         
-      } catch (error) {
-        console.error('Paciente',error)
-        console.log(token)
-      }
-    }
+    //   } catch (error) {
+    //     console.error('Paciente',error)
+    //     // console.log(token)
+    //   }
+    // }
     
-    // SE O USUÁRIO FOR MEDICO 
-    else if (token.role === "Medico") {
-      try {
-        const response = await api.get(`/Medicos/BuscarPorId?id=${user.id}`);
-        console.log('Funcionou, o usuário retornado é medico')
+    // // SE O USUÁRIO FOR MEDICO 
+    // else if (token.role === "Medico") {
+    //   try {
+    //     const response = await api.get(`/Medicos/BuscarPorId?id=${user.id}`);
+    //     console.log('Funcionou, o usuário retornado é medico')
 
-        setMedico(response.data);
-        setDataNascimento(response.data)
-        setCpf(response.data)
-        setLogradouro(response.data.endereco.logradouro);
-        setCep(response.data.endereco.cep);
-        setCidade(response.data.endereco.cidade);
+    //     setMedico(response.data);
+    //     setDataNascimento(response.data)
+    //     setCpf(response.data)
+    //     setLogradouro(response.data.endereco.logradouro);
+    //     setCep(response.data.endereco.cep);
+    //     setCidade(response.data.endereco.cidade);
         
-      } catch (error) {
-        console.error('Medico',error)
-      }
-    } 
+    //   } catch (error) {
+    //     console.error('Medico',error)
+    //   }
+    // } 
     
 
-    // SE O USUÁRIO FOR INDEFINIDO
-    else {
-      console.error('A função Search User Falhou.', error)
-    }
-    console.log(paciente);
-    console.log(medico);
-  }
+    // // SE O USUÁRIO FOR INDEFINIDO
+    // else {
+    //   console.error('A função Search User Falhou.', error)
+    // }
+    // // console.log(paciente);
+    // // console.log(medico);
+
 
   
   // await AsyncStorage.setItem("token", JSON.stringify())
@@ -136,9 +163,28 @@ export const PatientProfile = ({ navigation }) => {
     getCep();
   }, [cep]);
 
+  const BuscarDadosUsuario = () => {
+    const [usuario, setUsuario] = useState({});
+   
+    useEffect(() => {
+       const buscarDados = async () => {
+         try {
+           const token = await userDecodeToken(); // Supondo que userDecodeToken é uma função que retorna o token do usuário
+           const url = token.role === "Medico" ? "Medicos" : "Pacientes";
+           const response = await axios.get(`/${url}/BuscarPorId?id=${token.id}`);
+           setUsuario(response.data);
+         } catch (error) {
+           console.error('Erro ao buscar dados do usuário:', error);
+         }
+       };
+   
+       buscarDados();
+    }, []);
+  }
+
   useEffect(() => {
     profileLoad();
-    searchUser();
+    // searchUser();
   }, []);
 
   return (
@@ -151,13 +197,10 @@ export const PatientProfile = ({ navigation }) => {
 
         <DescriptionPassword description={user.email} />
 
-        {/* {user == "Paciente" ? (
-            <>
-              
         <InputBox
           placeholderTextColor={"#A1A1A1"}
           textLabel={"Data de nascimento:"}
-          placeholder={paciente.IdNavigation.datanascimento}
+          // placeholder={token.paciente.datanascimento}
           // placeholder={"Ex. 04/05/1999"}
           keyboardType="numeric"
           editable={true}
@@ -166,7 +209,7 @@ export const PatientProfile = ({ navigation }) => {
         <InputBox
           placeholderTextColor={"#A1A1A1"}
           textLabel={"CPF"}
-          placeholder={paciente.IdNavigation.cpf}
+          placeholder={user.cpf}
           keyboardType="numeric"
           maxLength={11}
           editable={true}
@@ -175,114 +218,7 @@ export const PatientProfile = ({ navigation }) => {
         <InputBox
           placeholderTextColor={"#A1A1A1"}
           textLabel={"Endereço"}
-          placeholder={paciente.endereco.logradouro}
-          editable={false}
-          fieldValue={logradouro}
-          fieldWidth={90}
-        />
-
-        <ContainerCepCidade>
-          <InputBox
-            placeholderTextColor={"#A1A1A1"}
-            textLabel={"CEP"}
-            placeholder={paciente.endereco.cep}
-            maxLength={8}
-            onChangeText={(text) => setCep(text)}
-            keyboardType="numeric"
-            editable={true}
-            fieldWidth={40}
-            fieldValue={cep}
-          />
-          <InputBox
-            placeholderTextColor={"#49B3BA"}
-            textLabel={"Cidade"}
-            placeholder={paciente.endereco.cidade}
-            editable={false}
-            fieldWidth={40}
-            fieldValue={cidade}
-          />
-        </ContainerCepCidade>
-            </>
-          ) : user == "Medico" ? (
-            <>
-              
-        <InputBox
-          placeholderTextColor={"#A1A1A1"}
-          textLabel={"Data de nascimento:"}
-          placeholder={doctor.IdNavigation.datanascimento}
-          // placeholder={"Ex. 04/05/1999"}
-          keyboardType="numeric"
-          editable={true}
-          fieldWidth={90}
-        />
-        <InputBox
-          placeholderTextColor={"#A1A1A1"}
-          textLabel={"CPF"}
-          placeholder={doctor.IdNavigation.cpf}
-          keyboardType="numeric"
-          maxLength={11}
-          editable={true}
-          fieldWidth={90}
-        />
-        <InputBox
-          placeholderTextColor={"#A1A1A1"}
-          textLabel={"Endereço"}
-          placeholder={doctor.endereco.logradouro}
-          editable={false}
-          fieldValue={logradouro}
-          fieldWidth={90}
-        />
-
-        <ContainerCepCidade>
-          <InputBox
-            placeholderTextColor={"#A1A1A1"}
-            textLabel={"CEP"}
-            placeholder={doctor.endereco.cep}
-            maxLength={8}
-            onChangeText={(text) => setCep(text)}
-            keyboardType="numeric"
-            editable={true}
-            fieldWidth={40}
-            fieldValue={cep}
-          />
-          <InputBox
-            placeholderTextColor={"#49B3BA"}
-            textLabel={"Cidade"}
-            placeholder={doctor.endereco.cidade}
-            editable={false}
-            fieldWidth={40}
-            fieldValue={cidade}
-          />
-        </ContainerCepCidade>
-            </>
-          ) : (
-            <></>
-          ) 
-          } */}
-
-          
-        <InputBox
-          placeholderTextColor={"#A1A1A1"}
-          textLabel={"Data de nascimento:"}
-          // placeholder={paciente.datanascimento}
-          // placeholder={"Ex. 04/05/1999"}
-          keyboardType="numeric"
-          editable={true}
-          fieldWidth={90}
-        />
-        <InputBox
-          placeholderTextColor={"#A1A1A1"}
-          textLabel={"CPF"}
-          placeholder={paciente.cpf}
-          keyboardType="numeric"
-          maxLength={11}
-          editable={true}
-          fieldWidth={90}
-        />
-        <InputBox
-          placeholderTextColor={"#A1A1A1"}
-          textLabel={"Endereço"}
-          // placeholder={endereco.logradouro}
+          placeholder={logradouro}
           editable={false}
           fieldValue={logradouro}
           fieldWidth={90}
