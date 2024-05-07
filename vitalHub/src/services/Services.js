6223f367bc2f18ec8f3fbf37ee033798ad971e6b
Vinícius Vieira De Andrade Correia <53685550837@info.local>
2024-05-07 16:09:05 -0300

    ...

--- vitalHub/src/services/Services.js
+++ vitalHub/src/services/Services.js
@@ -6,12 +6,9 @@ const porta = '4466';
 
 //declarar o ip da maquina 
 
-<<<<<<< HEAD
+
 const ipSenai = '172.16.39.109';
 
-=======
-const ip = '172.16.39.88';
->>>>>>> origin/mikael
 
 //definir a url padrao
 const apiUrlLocal = `http://${ipSenai}:${porta}/api`;
